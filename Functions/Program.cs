4dd3f79fb974e1b23eb0213186b9312cc60e30ac
Marcus <marcus.henriksson@restory.ai>
2022-10-10 10:16:04 +0200

    Add project files.

---
+++ Functions/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Innan funktionen");
+            myFunction();
+            myFunction();
+            Console.WriteLine("Efter funktionen");
+        }
+
+        public static void myFunction()
+        {
+            Console.WriteLine("Inuti en Metod!");
+        }
+
+    }
+}
