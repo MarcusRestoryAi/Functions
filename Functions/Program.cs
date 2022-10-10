b2fe6a96219294f15e59b51274ff57970a63c125
Marcus <marcus.henriksson@restory.ai>
2022-10-10 10:31:20 +0200

    Added Function oneOrTwo

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,15 +4,35 @@
     {
         static void Main(string[] args)
         {
+            //FUnktion myFunction
             Console.WriteLine("Innan funktionen");
             myFunction();
             myFunction();
             Console.WriteLine("Efter funktionen");
+
+            //Funktion oneOrTwo
+            oneOrTwo(1);
+            oneOrTwo(2);
+            oneOrTwo(3);
         }
 
+        //Funktion för att skriva ut meddelande till Console
         public static void myFunction()
         {
             Console.WriteLine("Inuti en Metod!");
+        }
+
+        //Funktion för att ta in en paramenter och avgöra om det är 1 eller 2
+        public static void oneOrTwo(int number)
+        {
+            //If sats för att kontrollera parameter
+            if (number == 1)
+            {
+                Console.WriteLine("Nummret är 1");
+            } else if (number == 2)
+            {
+                Console.WriteLine("Nummret är 2");
+            }
         }
 
     }
